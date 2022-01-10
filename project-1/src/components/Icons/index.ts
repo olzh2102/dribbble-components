f4e097b61e5b4eddc5a8520a7ed757998fc8531a
evitla <s.dinmuhamed@gmail.com>
2022-01-10 13:42:45 +0600

    feat: add CircleIcon

--- project-1/src/components/Icons/index.ts
+++ project-1/src/components/Icons/index.ts
@@ -1,2 +1,5 @@
 export { default as LineIcon } from './LineIcon';
+export { default as ArrowIcon } from './ArrowIcon';
+export { default as SquareIcon } from './SquareIcon';
+export { default as CircleIcon } from './CircleIcon';
 export { default as UploadImageIcon } from './UploadImageIcon';
