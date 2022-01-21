ede5af0e9e68d0020749c5ecb8c962e67f3f5255
evitla <s.dinmuhamed@gmail.com>
2022-01-21 21:41:33 +0600

    feat: add position styles

---
+++ project-1/src/constants/index.ts
@@ -0,0 +1,97 @@
+export const barBoxPositionStyles = {
+  top: {
+    flexDirection: 'row',
+    left: '50%',
+    top: '50px',
+    translateX: '-50%',
+  },
+
+  bottom: {
+    flexDirection: 'row',
+    left: '50%',
+    top: '500px',
+    translateX: '-50%',
+  },
+
+  left: {
+    flexDirection: 'column',
+    left: '0',
+    top: '50px',
+    translateX: '100%',
+  },
+
+  right: {
+    flexDirection: 'column',
+    left: '100%',
+    top: '50px',
+    translateX: '-200%',
+  },
+};
+
+export const shapeSelectorPositionStyles = {
+  top: {
+    flexDirection: 'row',
+    initialX: '0',
+    initialY: '0',
+    x: '0',
+    y: '40px',
+  },
+
+  bottom: {
+    flexDirection: 'row',
+    initialX: '0',
+    initialY: '-20px',
+    x: '0',
+    y: '-100px',
+  },
+
+  left: {
+    flexDirection: 'column',
+    initialX: '0',
+    initialY: '-20px',
+    x: '55px',
+    y: '-20px',
+  },
+
+  right: {
+    flexDirection: 'column',
+    initialX: '0',
+    initialY: '-20px',
+    x: '-85px',
+    y: '-20px',
+  },
+};
+
+export const colorSelectorPositionStyles = {
+  top: {
+    flexDirection: 'row',
+    initialX: '0',
+    initialY: '0',
+    x: '0',
+    y: '40px',
+  },
+
+  bottom: {
+    flexDirection: 'row',
+    initialX: '0',
+    initialY: '-225px',
+    x: '0',
+    y: '-275px',
+  },
+
+  left: {
+    flexDirection: 'column',
+    initialX: '0',
+    initialY: '-20px',
+    x: '55px',
+    y: '-20px',
+  },
+
+  right: {
+    flexDirection: 'column',
+    initialX: '-150',
+    initialY: '-20px',
+    x: '-255px',
+    y: '-20px',
+  },
+};
