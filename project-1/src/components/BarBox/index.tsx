a31592bdc90ee44dbd670c9f6056e19fc40d9297
evitla <s.dinmuhamed@gmail.com>
2022-01-21 21:45:21 +0600

    feat: change BarBox position by pressing arrow keys

--- project-1/src/components/BarBox/index.tsx
+++ project-1/src/components/BarBox/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { RgbaColor } from "react-colorful";
+import { useContext, useEffect, useState } from 'react';
+import { RgbaColor } from 'react-colorful';
 
 import { Shapes } from '../../types';
 import { toHex } from '../../utils';
@@ -8,26 +8,52 @@ import ColorSelector from '../ColorSelector';
 import ImageUploader from '../ImageUploader';
 
 import Box from './style';
+import { SetBoxPositionContext } from '../../hooks/useSetBoxPosition';
+import { barBoxPositionStyles as styles } from '../../constants';
 
 const BarBox = () => {
   const [shape, setShape] = useState<Shapes>('line');
-  const [color, setColor] = useState<RgbaColor>({"r":64,"g":45,"b":104,"a":1});
-  const [hoverColor, setHoverColor] = useState<RgbaColor>({"r":64,"g":45,"b":104,"a":0.6});
-  const [images, setImages] = useState<File[]>([])
+  const [color, setColor] = useState<RgbaColor>({ r: 64, g: 45, b: 104, a: 1 });
+  const [hoverColor, setHoverColor] = useState<RgbaColor>({
+    r: 64,
+    g: 45,
+    b: 104,
+    a: 0.6,
+  });
+  const [images, setImages] = useState<File[]>([]);
+
+  const { position, setPosition } = useContext(SetBoxPositionContext);
+
+  const [state, setState] = useState<any>(styles[position]);
+
+  const pressKey = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowUp') setPosition('top');
+    if (e.key === 'ArrowDown') setPosition('bottom');
+    if (e.key === 'ArrowLeft') setPosition('left');
+    if (e.key === 'ArrowRight') setPosition('right');
+  };
+
+  window.addEventListener('keydown', pressKey);
+
+  useEffect(() => {
+    setState(styles[position]);
+  }, [position]);
 
   return (
-    <Box>
+    <Box state={state}>
       <div className="section">
         <div className="items">
-          <ShapeSelector 
-            color={toHex(`rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`)} 
-            shape={shape} 
-            onChange={setShape} 
+          <ShapeSelector
+            color={toHex(
+              `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
+            )}
+            shape={shape}
+            onChange={setShape}
           />
         </div>
 
         <div className="items">
-          <ColorSelector 
+          <ColorSelector
             onChange={setColor}
             color={color}
             hoverColor={hoverColor}
@@ -38,9 +64,11 @@ const BarBox = () => {
 
       <div className="section">
         <div className="items">
-          <ImageUploader 
-            onImageUpload={setImages} 
-            color={toHex(`rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`)}
+          <ImageUploader
+            onImageUpload={setImages}
+            color={toHex(
+              `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
+            )}
           />
         </div>
       </div>
@@ -49,4 +77,3 @@ const BarBox = () => {
 };
 
 export default BarBox;
-
