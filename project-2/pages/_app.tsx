c017c3f620afb4cc30953241d0e28f0d660dbd8b
evitla <s.dinmuhamed@gmail.com>
2022-01-26 19:40:32 +0600

    feat: add QueryClientProvider

--- project-2/pages/_app.tsx
+++ project-2/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '.';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
+);
 
-export default MyApp
+export default App;
