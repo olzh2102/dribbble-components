b2e2e52f6693aafe3a3bd17e94a29c8a8c816f7f
evitla <s.dinmuhamed@gmail.com>
2022-01-26 19:41:14 +0600

    feat: fetch data with useQuery

--- project-2/pages/index.tsx
+++ project-2/pages/index.tsx
@@ -1,9 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { QueryClient, useQuery } from 'react-query';
+
+export const queryClient = new QueryClient();
+
+const URL =
+  'https://countriesnow.space/api/v0.1/countries/info?returns=flag,dialCode';
 
 const Home: NextPage = () => {
+  const query = useQuery('countries', () =>
+    fetch(URL).then((res) => res.json())
+  );
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +21,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(6, 1fr)',
+          gap: '1rem',
+        }}
+      >
+        {query.data?.data.map((c: any, index: any) => (
+          <div
+            key={index}
+            style={{ border: '1px solid black', padding: '20px' }}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+            <h2>Code:</h2>
+            <span>{c.dialCode}</span>
+            <h2>Flag:</h2>
+            <img src={c.flag} alt={c.name} style={{ width: '100px' }} />
+            <h2>Name:</h2>
+            <span>{c.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
