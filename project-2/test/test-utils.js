ea75877be57f7027675d54c9659b87647687dff9
Olzhas Kurikov <okur@hellofresh.com>
2022-01-31 23:23:35 +0100

    fix: create QueryProvider wrapper

--- project-2/test/test-utils.js
+++ project-2/test/test-utils.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { render as rtlRender } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function Providers({ children }) {
     return children
@@ -10,9 +13,17 @@ function render(
     ui,
     options = {}
 ) {
+    function Wrapper({ children }) {
+        return (
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        )
+    }
+
     return rtlRender(
         ui,
-        { wrapper: Providers, ...options }
+        { wrapper: Wrapper, ...options }
     )
 }
 
