728f9dea71e18bc02306edccc3c7e0062cf27614
evitla <s.dinmuhamed@gmail.com>
2022-02-02 19:24:24 +0600

    refactor: resolve conflicts

--- project-2/components/app/index.tsx
+++ project-2/components/app/index.tsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import { useDebounce, useFetchCountries } from '../../hooks'
-import SearchInput from '../search-input'
-import { StyledContainer } from './style'
+import { useDebounce, useFetchCountries } from '../../hooks';
+import SearchInput from '../search-input';
+import { StyledContainer } from './style';
 
 const App = () => {
-    const { countries, isLoading, isError } = useFetchCountries()
-    console.log('Countries: ', countries)
+  const { countries, isLoading, isError } = useFetchCountries();
+  console.log('Countries: ', countries);
 
-    const [searchTerm, setSearchTerm] = useState('')
-    const debouncedValue = useDebounce(searchTerm)
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedValue = useDebounce(searchTerm);
 
-    useEffect(() => {
-        // TODO: list re-render on searchTerm
-    }, [debouncedValue])
+  useEffect(() => {
+    // TODO: list re-render on searchTerm
+  }, [debouncedValue]);
 
-    return (    
-        <StyledContainer data-testid="app">
-            <SearchInput
-                onChange={(val: string) => setSearchTerm(val)}
-                placeholder="Search countries"
-                value={searchTerm}
-            />
-            
-            {
-                isLoading 
-                    // TODO: replace loading div with Skeleton component
-                    ? <div>Countries are loading...</div> 
-                    : isError 
-                    ? <div>Error happened</div>
-                    // TODO: replace div with Country component inside iterate over countries
-                    : <div>Countries will go here</div>
-            }
-        </StyledContainer>
-    )
-}
+  return (
+    <StyledContainer data-testid="app">
+      <SearchInput
+        onChange={(val: string) => setSearchTerm(val)}
+        placeholder="Search countries"
+        value={searchTerm}
+      />
+
+      {isLoading ? (
+        // TODO: replace loading div with Skeleton component
+        <div>Countries are loading...</div>
+      ) : isError ? (
+        <div>Error happened</div>
+      ) : (
+        // TODO: replace div with Country component inside iterate over countries
+        <div>Countries will go here</div>
+      )}
+    </StyledContainer>
+  );
+};
 
 export default App;
--- project-2/constants/index.ts
+++ project-2/constants/index.ts
@@ -1,3 +1,5 @@
+export const URL =
+  'https://countriesnow.space/api/v0.1/countries/info?returns=flag,dialCode';
 export const QUERY_KEY_COUNTRIES = 'COUNTRIES';
 export const DEFAULT_DELAY_TIME = 1000;
 export const URL_COUNTRIES = 'https://countriesnow.space/api/v0.1/countries/info?returns=flag,dialCode,codes';
