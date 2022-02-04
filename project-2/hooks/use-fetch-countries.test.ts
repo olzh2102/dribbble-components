1a15b12c163dc3f2fea08e496d2ca481f94b4c76
evitla <s.dinmuhamed@gmail.com>
2022-02-04 20:15:53 +0600

    test: assertion countries length

--- project-2/hooks/use-fetch-countries.test.ts
+++ project-2/hooks/use-fetch-countries.test.ts
@@ -3,17 +3,18 @@ import { renderHook, QueryClientWrapper } from '../test/test-utils';
 import useFetchCountries from './use-fetch-countries';
 
 describe('use fetch countries hook', () => {
-    function setup() {
-        return renderHook(
-            () => useFetchCountries(), 
-            { wrapper: QueryClientWrapper }
-        )
-    }
+  function setup() {
+    return renderHook(() => useFetchCountries(), {
+      wrapper: QueryClientWrapper,
+    });
+  }
 
-    it('should return 3 countries', async () => {
-        const { result, waitFor } = setup()
-        // TODO: your assertions goes here
-    })
+  it('should return 3 countries', async () => {
+    const { result, waitFor } = setup();
 
-    it.todo('should fail and show error message inside application')
-})
+    await waitFor(() => result.current.isSuccess);
+    expect(result.current.data.length).toBe(3);
+  });
+
+  it.todo('should fail and show error message inside application');
+});
