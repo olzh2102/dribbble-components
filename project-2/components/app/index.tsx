9acd0bdf6f7db114ea5f7645de96b94768be9843
evitla <s.dinmuhamed@gmail.com>
2022-02-06 17:40:18 +0600

    refactor: change import paths

--- project-2/components/app/index.tsx
+++ project-2/components/app/index.tsx
@@ -11,59 +11,53 @@ import { CrossIcon } from '../icons';
 import { StyledContainer } from './style';
 
 const App = () => {
-    const { countries, isLoading, isError } = useFetchCountries();
+  const { countries, isLoading, isError } = useFetchCountries();
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const debouncedValue = useDebounce(searchTerm);
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedValue = useDebounce(searchTerm);
 
-    const [selectedCountry, setSelectedCountry] = useState({} as any);
+  const [selectedCountry, setSelectedCountry] = useState({} as any);
 
-    const handleRemoveCountry = () => {
-        if (!isEmpty(selectedCountry))
-            setSelectedCountry({})
-    }
+  const handleRemoveCountry = () => {
+    if (!isEmpty(selectedCountry)) setSelectedCountry({});
+  };
 
-    useEffect(() => {
-        // TODO: list re-render on searchTerm
-    }, [debouncedValue])
+  useEffect(() => {
+    // TODO: list re-render on searchTerm
+  }, [debouncedValue]);
 
-    return (    
-        <StyledContainer data-testid="app">
-            <SearchInput
-                onChange={setSearchTerm}
-                placeholder="Search countries"
-                value={searchTerm}
-            />
+  return (
+    <StyledContainer data-testid="app">
+      <SearchInput
+        onChange={setSearchTerm}
+        placeholder="Search countries"
+        value={searchTerm}
+      />
 
-            {isLoading ? (
-                <Skeleton count={5} />
-                ) : isError ? (
-                <div>Error happened</div>
-                ) : (
-                <CountryList 
-                    countries={countries} 
-                    onSelect={setSelectedCountry} 
-                    selectedCountry={selectedCountry} 
-                />
-            )}
-            
-            <div className="actions">
-                <Button
-                    as="link"
-                    to="/"
-                    label="NEXT"
-                    variant="outlined"
-                />
+      {isLoading ? (
+        <Skeleton count={5} />
+      ) : isError ? (
+        <div>Error happened</div>
+      ) : (
+        <CountryList
+          countries={countries}
+          onSelect={setSelectedCountry}
+          selectedCountry={selectedCountry}
+        />
+      )}
 
-                <Button
-                    onClick={handleRemoveCountry}
-                    icon={<CrossIcon />}
-                    disabled={Object.keys(selectedCountry).length == 0}
-                    variant="contained"
-                />
-            </div>
-        </StyledContainer>
-    );
+      <div className="actions">
+        <Button as="link" to="/" label="NEXT" variant="outlined" />
+
+        <Button
+          onClick={handleRemoveCountry}
+          icon={<CrossIcon />}
+          disabled={Object.keys(selectedCountry).length == 0}
+          variant="contained"
+        />
+      </div>
+    </StyledContainer>
+  );
 };
 
 export default App;
