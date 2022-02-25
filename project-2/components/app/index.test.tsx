0cde82c1329844eb1d14cddfed8b2ba5cf809e17
evitla <s.dinmuhamed@gmail.com>
2022-02-25 21:09:35 +0600

    test: render skeletons while fetching countries

--- project-2/components/app/index.test.tsx
+++ project-2/components/app/index.test.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render, screen } from '../../test/test-utils';
+import { waitFor, render, screen } from '../../test/test-utils';
 
 import App from '.';
 
 describe('<Application />', () => {
-    it('renders Application', () => {
-        render(<App />);
-        const appWrapper = screen.getByTestId('app');
-        expect(appWrapper).toBeInTheDocument()
-    })
-})
+  it('renders Application', () => {
+    render(<App />);
+    const appWrapper = screen.getByTestId('app');
+    expect(appWrapper).toBeInTheDocument();
+  });
+
+  it('should render loading component while fetching countries', async () => {
+    render(<App />);
+
+    expect(screen.getByTestId('skeletons')).toBeInTheDocument();
+
+    await waitFor(() => expect(screen.queryByTestId('skeletons')).toBeNull());
+  });
+});
