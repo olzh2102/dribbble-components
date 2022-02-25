0db337bb1980cb3689ca9e0855d679b33ec6034a
evitla <s.dinmuhamed@gmail.com>
2022-02-25 21:08:18 +0600

    refactor: add data-testid to skeletons wrapper

--- project-2/components/skeleton/index.tsx
+++ project-2/components/skeleton/index.tsx
@@ -1,7 +1,7 @@
 import ContentLoader from 'react-content-loader';
 
 const Skeleton = ({ count }: { count: number }) => (
-  <>
+  <div data-testid="skeletons">
     {Array.from({ length: count }, (_, i) => (
       <ContentLoader
         key={i}
@@ -11,13 +11,12 @@ const Skeleton = ({ count }: { count: number }) => (
         viewBox="0 0 280 40"
         backgroundColor="#f3f3f3"
         foregroundColor="#dadada"
-        data-testid="skeleton"
       >
         <circle cx="15" cy="15" r="15" />
         <rect x="38" y="2" rx="6" ry="6" width="220" height="25" />
       </ContentLoader>
     ))}
-  </>
+  </div>
 );
 
 export default Skeleton;
