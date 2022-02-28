3c2e377d719b0c6cb4ac05711ace9efd96dce824
evitla <s.dinmuhamed@gmail.com>
2022-02-28 17:36:45 +0600

    feat: msw basic setup

--- project-3/jest.setup.js
+++ project-3/jest.setup.js
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
+import { server } from './__api_mocks__/mswServer';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
