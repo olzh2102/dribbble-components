f9078d6851678a54a4697b55534183260c9fd70b
evitla <s.dinmuhamed@gmail.com>
2022-03-01 11:43:33 +0600

    refactor: add constants

---
+++ project-3/constants/index.ts
@@ -0,0 +1,5 @@
+export const WEATHER_API_KEY = process.env.NEXT_PUBLIC_API_KEY || '';
+export const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5';
+export const CURRENT_WEATHER_URL = `${BASE_WEATHER_URL}/weather`;
+export const ONECALL_WEATHER_URL = `${BASE_WEATHER_URL}/onecall`;
+export const QUERY_KEY_WEATHER = 'weather';
