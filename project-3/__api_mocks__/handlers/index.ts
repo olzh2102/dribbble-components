7019414ac0cf28c39d5c2bada65f3c47ad03cc9c
evitla <s.dinmuhamed@gmail.com>
2022-03-02 12:07:56 +0600

    feat: add handlers for testing weather api call

---
+++ project-3/__api_mocks__/handlers/index.ts
@@ -0,0 +1,46 @@
+import { rest } from 'msw';
+
+import { CURRENT_WEATHER_URL, ONECALL_WEATHER_URL } from '../../constants';
+
+export const handlers = [
+  rest.get(CURRENT_WEATHER_URL, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        coord: {
+          lat: 51,
+          lon: 71,
+        },
+      })
+    )
+  ),
+  rest.get(ONECALL_WEATHER_URL, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        current: {
+          temp: 22,
+          humidity: 75,
+        },
+        hourly: [
+          {
+            temp: 25,
+            humidity: 70,
+          },
+          {
+            temp: 22,
+            humidity: 75,
+          },
+          {
+            temp: 20,
+            humidity: 74,
+          },
+        ],
+        lat: 51,
+        lon: 71,
+        timezone: 'Asia/Almaty',
+        timezone_offset: 21600,
+      })
+    )
+  ),
+];
