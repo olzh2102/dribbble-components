0444c9353dab215f160bce425dc098c0d428d753
evitla <s.dinmuhamed@gmail.com>
2022-03-02 12:09:14 +0600

    feat: create QueryClientProvider wrapper

--- project-3/test/test-utils.js
+++ project-3/test/test-utils.js
@@ -1,12 +1,41 @@
+import next from 'next';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { render as rtlRender } from '@testing-library/react';
 
-function Providers({ children }) {
-  return children;
-}
+const queryClient = new QueryClient();
 
 function render(ui, options = {}) {
-  return rtlRender(ui, { wrapper: Providers, ...options });
+  function Wrapper({ children }) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    );
+  }
+
+  return rtlRender(ui, { wrapper: Wrapper, ...options });
 }
+
+export const createWrapper = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+    },
+  });
+
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+};
+
+export const QueryClientWrapper = ({ children }) => (
+  <QueryClientProvider client={new QueryClient}>
+    {children}
+  </QueryClientProvider>
+)
 
 export * from '@testing-library/react';
 export { renderHook } from '@testing-library/react-hooks';
