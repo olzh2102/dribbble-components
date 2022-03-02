5bffc2401706a0551e10637c13f2ba91d7cc4571
evitla <s.dinmuhamed@gmail.com>
2022-03-02 16:28:36 +0600

    test: fail case

--- project-3/hooks/useFetchWeather.test.ts
+++ project-3/hooks/useFetchWeather.test.ts
@@ -1,4 +1,7 @@
+import { setLogger } from 'react-query';
 import { renderHook, createWrapper } from '../test/test-utils';
+import { server } from '../__api_mocks__/mswServer';
+import { networkErrorHandlers } from '../__api_mocks__/handlers/networkError';
 
 import useFetchWeather from './useFetchWeather';
 
@@ -18,5 +21,21 @@ describe('use fetch weather hook', () => {
     await waitFor(() => result.current.isSuccess);
 
     expect(result.current.data).toHaveProperty('hourly');
+  });
+
+  it('should fail', async () => {
+    setLogger({
+      log: console.log,
+      warn: console.warn,
+      // ✅ no more errors on the console
+      error: () => {},
+    })
+
+    server.use(...networkErrorHandlers);
+    const { result, waitFor } = setup();
+
+    await waitFor(() => result.current.isError);
+
+    expect((result as any).current.error.message).toEqual('Network Error');
   });
 });
