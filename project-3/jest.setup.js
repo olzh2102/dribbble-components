491b9d77c5e350b3d441db6592bd64ea0db11fbc
evitla <s.dinmuhamed@gmail.com>
2022-03-03 12:44:44 +0600

    refactor: rename filenames using kebab-case

--- project-3/jest.setup.js
+++ project-3/jest.setup.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import { server } from './__api_mocks__/mswServer';
+import { server } from './__api_mocks__/msw-server';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
