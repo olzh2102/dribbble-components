5aaf068b91eed0a04f418081c78ed7f107a99c50
evitla <s.dinmuhamed@gmail.com>
2022-03-06 22:25:38 +0600

    styles: set autocomplete width

--- project-3/stories/autocomplete/index.tsx
+++ project-3/stories/autocomplete/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import MuiAutocomplete, { AutocompleteProps as MuiAutocompleteProps } from '@mui/material/Autocomplete';
+import { AutocompleteProps as MuiAutocompleteProps } from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
-const Autocomplete = (props: AutocompleteProps) => {   
-    return (
-        <MuiAutocomplete 
-            {...props}
-            options={[''].concat(props.options)}
-            onChange={(e, val) => props.onChange(val)}
-            renderInput={params => (
-                <TextField 
-                    {...params}
-                    label={props.label}
-                />
-            )}
-        />
-    )
-}
+import { MuiAutocompleteStyled } from './styled';
 
-export default Autocomplete
+const Autocomplete = (props: AutocompleteProps) => {
+  return (
+    <MuiAutocompleteStyled
+      {...props}
+      options={[''].concat(props.options)}
+      onChange={(e, val) => props.onChange(val)}
+      renderInput={(params) => <TextField {...params} label={props.label} />}
+    />
+  );
+};
 
-type AutocompleteProps = Omit<MuiAutocompleteProps<any, any, any, any>, 'renderInput'> & {
-    label: string;
-    onChange: (val: any) => void | null;
-}
+export default Autocomplete;
+
+type AutocompleteProps = Omit<
+  MuiAutocompleteProps<any, any, any, any>,
+  'renderInput'
+> & {
+  label: string;
+  onChange: (val: any) => void | null;
+};
---
+++ project-3/stories/autocomplete/styled.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export const MuiAutocompleteStyled = styled(Autocomplete)`
+  width: 15rem;
+`;
