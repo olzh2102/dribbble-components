79321ec9e39ad48e849b42979368b05b8b12444b
Olzhas Kurikov <okur@hellofresh.com>
2022-03-20 00:04:31 +0100

    draft: hourly statistics

--- project-3/components/statistics/index.tsx
+++ project-3/components/statistics/index.tsx
@@ -1,21 +1,31 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Box } from '@mui/material';
 import WEATHER_DATA_MOCK from './data-mock';
 
 const Statistics = () => {
   return (
-    <Card variant="outlined">
-      <CardContent sx={{ textAlign: 'left' }}>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Astana
-        </Typography>
-        <Typography variant="h5" component="div">
-          {WEATHER_DATA_MOCK.current.temp}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {WEATHER_DATA_MOCK.current.weather[0].description}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      <Card variant="outlined">
+        <CardContent sx={{ textAlign: 'left' }}>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Astana
+          </Typography>
+          <Typography variant="h5" component="div">
+            {WEATHER_DATA_MOCK.current.temp}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {WEATHER_DATA_MOCK.current.weather[0].description}
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Typography sx={{ textAlign: 'left' }} variant="body2" component="div">
+        HOURLY STATISTICS:
+      </Typography>
+
+      <Card variant="outlined">
+        <CardContent>hourly chart will go here</CardContent>
+      </Card>
+    </Box>
   );
 };
 
