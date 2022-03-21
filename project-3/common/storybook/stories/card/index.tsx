d9384d186f8b2d00f04a17ed092ba3d38d15c7ac
evitla <s.dinmuhamed@gmail.com>
2022-03-21 23:02:04 +0600

    feat: add Card content

--- project-3/common/storybook/stories/card/index.tsx
+++ project-3/common/storybook/stories/card/index.tsx
@@ -1,19 +1,34 @@
-import { CardHeader, CardProps } from '@mui/material';
-import { StyledCard } from './style';
+import { CardContent, CardHeader, CardMedia, CardProps } from '@mui/material';
+import { StyledCard, TypographyWithIcon } from './style';
 
 const Card = (props: TCardProps) => {
   return (
-    <StyledCard sx={props.sx}>
+    <StyledCard sx={props.sx} orientation={props.orientation || 'vertical'}>
       <CardHeader title={props.title} subheader={props.subTitle} />
-      {props.children}
+      {props.withInfo && (
+        <CardContent>
+          <TypographyWithIcon variant="h5">
+            {props.temperature}
+            <img src={`${props.icon}`} alt="Weather icon" />
+          </TypographyWithIcon>
+        </CardContent>
+      )}
+      {props.withImage && (
+        <CardMedia component="img" image={props.image} alt="Card image" />
+      )}
     </StyledCard>
   );
 };
 
 export default Card;
 
-export type TCardProps = CardProps & {
+export type TCardProps = Omit<CardProps, 'variant'> & {
   title: string;
   subTitle: string;
-  children: JSX.Element;
+  temperature: string;
+  icon: string;
+  orientation: 'horizontal' | 'vertical';
+  image: string;
+  withImage: boolean;
+  withInfo: boolean;
 };
