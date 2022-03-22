64467ed28ea485e53d360324aeb8ed0aafc839ce
Olzhas Kurikov <okur@hellofresh.com>
2022-03-22 23:22:35 +0100

    feat: line change to bar chart

--- project-3/components/statistics/index.tsx
+++ project-3/components/statistics/index.tsx
@@ -1,25 +1,9 @@
 import { Card, CardContent, Typography, Box } from '@mui/material';
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 import WEATHER_DATA_MOCK from './data-mock';
 
 const Statistics = () => {
-  const data = [
-    { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 300, pv: 2400, amt: 2400 },
-    { name: 'Page C', uv: 300, pv: 2400, amt: 2400 },
-    { name: 'Page D', uv: 200, pv: 2400, amt: 2400 },
-    { name: 'Page E', uv: 280, pv: 2400, amt: 2400 },
-    { name: 'Page F', uv: 170, pv: 2400, amt: 2400 },
-  ];
-
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
       <Card variant="outlined">
@@ -40,22 +24,13 @@ const Statistics = () => {
         HOURLY STATISTICS:
       </Typography>
 
-      <Card variant="outlined">
-        <CardContent>
-          <LineChart
-            width={600}
-            height={300}
-            data={data}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          >
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-          </LineChart>
-        </CardContent>
-      </Card>
+      <BarChart width={730} height={250} data={WEATHER_DATA_MOCK.hourly}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="humidity" tick={false} />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="humidity" fill="#8884d8" />
+      </BarChart>
     </Box>
   );
 };
