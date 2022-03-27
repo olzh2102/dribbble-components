9c1b9aac25544df3889603f446261817f6b45bcd
Olzhas Kurikov <okur@hellofresh.com>
2022-03-27 13:00:12 +0200

    draft: autocomplete to select city

--- project-3/components/index.tsx
+++ project-3/components/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { pipe, prop, mergeLeft } from 'ramda';
 import { Box } from '@mui/material';
@@ -8,12 +9,15 @@ import useFetchCoordinates from '@hooks/use-fetch-coordinates';
 import useFetchStatistics from '@hooks/use-fetch-statistics';
 
 import Statistics from './statistics';
+import Autocomplete from '@common/storybook/stories/autocomplete';
 
 const App = () => {
   const dispatch = useDispatch();
 
   const excludes = ['daily', 'minutely', 'alerts'] as TExclude[];
   const cityName = 'Astana';
+  const [city, setCity] = useState({});
+  console.log(city);
 
   const { data: res } = useFetchCoordinates(
     { cityName, excludes },
@@ -36,7 +40,15 @@ const App = () => {
 
   return (
     <Box display="flex" sx={{ gap: '32px' }}>
-      <Box>Autocomplete Search component</Box>
+      <Box>
+        <Autocomplete
+          label="Select a city"
+          value={city}
+          getOptionLabel={(option: any) => option.label || ''}
+          options={[{ label: 'Nur-Sultan', name: 'Astana' }]}
+          onChange={setCity}
+        />
+      </Box>
 
       <Box>
         {isLoading ? (
