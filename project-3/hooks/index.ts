671fd4abbdb02ea7191ea88782c134851b6c9ce4
Olzhas Kurikov <okur@hellofresh.com>
2022-04-03 11:13:54 +0200

    feat: hook to get coordinates of current location

---
+++ project-3/hooks/index.ts
@@ -0,0 +1,9 @@
+import useFetchCoordinates from './use-fetch-coordinates';
+import useFetchStatistics from './use-fetch-statistics';
+import useCurrentLocationCoordinates from './use-current-location-coordinates';
+
+export {
+  useFetchCoordinates,
+  useFetchStatistics,
+  useCurrentLocationCoordinates,
+};
