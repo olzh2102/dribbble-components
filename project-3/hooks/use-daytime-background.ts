861aa12cf4a26fcbe84b8312ac7e447f0c7af8a0
Olzhas Kurikov <okur@hellofresh.com>
2022-04-07 17:38:10 +0200

    draft: map of daytime segments

---
+++ project-3/hooks/use-daytime-background.ts
@@ -0,0 +1,11 @@
+const daySegments = {
+  22: 'night',
+  18: 'evening',
+  12: 'afternoon',
+  5: 'morning',
+  0: 'early morning',
+};
+
+const useDaytimeBackground = () => {
+  const now = new Date();
+};
