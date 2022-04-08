c9149a90d2007ad7eca7339de671ed85f5a2ecb9
evitla <s.dinmuhamed@gmail.com>
2022-04-08 14:43:39 +0600

    refactor: use Chart component in app

--- project-3/components/statistics/humidity-chart.tsx
+++ project-3/components/statistics/humidity-chart.tsx
@@ -1,13 +1,16 @@
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import Chart from '@common/storybook/stories/chart';
 
 export default function HumidityChart({ data }: any) {
   return (
-    <BarChart id="humidity-chart" width={730} height={250} data={data?.hourly}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="humidity" tick={false} />
-      <YAxis />
-      <Tooltip />
-      <Bar dataKey="humidity" fill="#8884d8" />
-    </BarChart>
+    <Chart
+      id="humidity-chart"
+      width={730}
+      height={250}
+      data={data?.hourly}
+      chartType="bar"
+      barProps={{ dataKey: 'humidity', fill: '#8884d8' }}
+      cartesianGridProps={{ strokeDasharray: '3 3' }}
+      xAxisProps={{ dataKey: 'humidity', tick: false }}
+    />
   );
 }
--- project-3/components/statistics/temperature-chart.tsx
+++ project-3/components/statistics/temperature-chart.tsx
@@ -1,40 +1,36 @@
-import {
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  AreaChart,
-  Area,
-} from 'recharts';
+import Chart from '@common/storybook/stories/chart';
 
 export default function TemperatureChart({ data }: any) {
   return (
-    <AreaChart width={730} height={250} data={data?.hourly}>
+    <Chart
+      width={730}
+      height={250}
+      chartType="area"
+      data={data?.hourly}
+      areaProps={{
+        dataKey: 'temp',
+        type: 'monotone',
+        stroke: '#82ca9d',
+        fill: 'url(#colorUv)',
+        fillOpacity: 1,
+      }}
+      cartesianGridProps={{ strokeDasharray: '3 3' }}
+      xAxisProps={{ dataKey: 'dt', tick: renderCustomAxisTick }}
+      tooltipProps={{
+        labelFormatter: (value: number) =>
+          `hours: ${new Date(value * 1000)
+            .getHours()
+            .toString()
+            .padStart(2, '0')}`,
+      }}
+    >
       <defs>
         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
           <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
           <stop offset="95%" stopColor="#82ca9d" stopOpacity={0.05} />
         </linearGradient>
       </defs>
-      <Area
-        type="monotone"
-        dataKey="temp"
-        stroke="#82ca9d"
-        fillOpacity={1}
-        fill="url(#colorUv)"
-      />
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="dt" tick={renderCustomAxisTick} />
-      <YAxis />
-      <Tooltip
-        labelFormatter={(value: number) =>
-          `hours: ${new Date(value * 1000)
-            .getHours()
-            .toString()
-            .padStart(2, '0')}`
-        }
-      />
-    </AreaChart>
+    </Chart>
   );
 }
 
