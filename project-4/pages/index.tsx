78aa9d911f14a22091312997dd854bb3f808a2ef
evitla <s.dinmuhamed@gmail.com>
2022-04-19 13:33:37 +0600

    init: start project 4

---
+++ project-4/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Video Chat</title>
+        <meta name="description" content="Video Chat App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
