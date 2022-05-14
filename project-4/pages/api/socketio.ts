dca8c9934750f4b0f1cca051bcf67c1967f68ea2
evitla <s.dinmuhamed@gmail.com>
2022-05-14 15:33:14 +0600

    feat: create sample socket io

---
+++ project-4/pages/api/socketio.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest } from 'next';
+import { Server as ServerIO } from 'socket.io';
+import { NextApiResponseServerIO } from '../../common/types';
+
+const socketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running');
+  } else {
+    console.log('Socket is initializing');
+    const httpServer = res.socket.server;
+    const io = new ServerIO(httpServer, { path: '/api/socketio' });
+    res.socket.server.io = io;
+
+    io.on('connection', (socket) => {
+      console.log('connected');
+      socket.on('join-room', ({ room }) => {
+        socket.emit('message', { msg: `Welcome to the room ${room}` });
+      });
+
+      socket.on('disconnect', () => {});
+    });
+  }
+
+  res.end();
+};
+
+export default socketHandler;
