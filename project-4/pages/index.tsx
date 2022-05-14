966a5e1b634d233307b2fd3d7b900abdaf466a07
evitla <s.dinmuhamed@gmail.com>
2022-05-14 15:36:46 +0600

    feat: check socket io

--- project-4/pages/index.tsx
+++ project-4/pages/index.tsx
@@ -1,11 +1,33 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import { JoinRoom, WelcomeContainer } from '../components';
+import { useSocketContext } from '../hooks';
 import { CameraIcon } from '../assets/icons';
 import { Button, JoinButton } from './style';
 
 const Home: NextPage = () => {
+  const room = 'some-room-id';
+
+  const { socket } = useSocketContext({ room });
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on('message', (message) => {
+      setMessage(message.msg);
+    });
+
+    return () => {
+      socket.off('message');
+    };
+  }, [socket]);
+
+  const joinRoom = () => {
+    console.log(message);
+  };
+
   return (
     <WelcomeContainer>
       <div className="mt-5 sm:flex sm:justify-center lg:justify-start">
@@ -18,7 +40,9 @@ const Home: NextPage = () => {
           </Link>
         </div>
         <JoinRoom />
-        <JoinButton disabled={true}>Join</JoinButton>
+        <JoinButton disabled={false} onClick={joinRoom}>
+          Join
+        </JoinButton>
       </div>
     </WelcomeContainer>
   );
