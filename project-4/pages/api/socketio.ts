e5d90f84678d8abb4f1c6e63302d29e570e3a2fd
evitla <s.dinmuhamed@gmail.com>
2022-05-30 20:41:47 +0600

    refactor: remove consoles

--- project-4/pages/api/socketio.ts
+++ project-4/pages/api/socketio.ts
@@ -3,26 +3,24 @@ import { Server as ServerIO } from 'socket.io';
 import { NextApiResponseServerIO } from '../../common/types';
 
 const socketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running');
-  } else {
+  if (!res.socket.server.io) {
     console.log('Socket is initializing');
+
     const httpServer = res.socket.server;
     const io = new ServerIO(httpServer, { path: '/api/socketio' });
     res.socket.server.io = io;
 
     io.on('connection', (socket) => {
       console.log('connected');
+
       socket.on('join-room', ({ roomId, userId }) => {
         console.log('USER ID: ', userId);
         console.log('ROOM ID: ', roomId);
+
         socket.join(roomId);
         socket.to(roomId).emit('member-joined', userId);
 
         socket.on('disconnect', () => {
-          console.log('is member leaving???');
-          console.log('is member leaving???', userId);
-
           socket.to(roomId).emit('member-left', userId);
         });
 
