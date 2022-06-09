8b1a733a5a76c8587d01d08a0aeda5db054d2863
evitla <s.dinmuhamed@gmail.com>
2022-06-09 19:07:35 +0600

    refactor: decrease control panel buttons size

--- project-4/assets/icons/hang-up.tsx
+++ project-4/assets/icons/hang-up.tsx
@@ -2,7 +2,7 @@ const HangUpIcon = ({ className }: { className?: string }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className={className || 'h-6 w-6'}
+      className={className || 'h-5 w-5'}
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
