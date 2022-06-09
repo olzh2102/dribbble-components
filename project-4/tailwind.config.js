29609cbff01c713841842eb90cd76a59afb41ca8
evitla <s.dinmuhamed@gmail.com>
2022-06-09 19:07:15 +0600

    feat: add assets folder to tailwind config

--- project-4/tailwind.config.js
+++ project-4/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./assets/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
