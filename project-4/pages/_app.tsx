d7f6bf859cf388a43c32a008ba5673fdff3b0775
evitla <s.dinmuhamed@gmail.com>
2022-08-15 21:11:07 +0600

    refactor: remove unnecessary lines

--- project-4/pages/_app.tsx
+++ project-4/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext } from 'react';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
@@ -7,25 +7,12 @@ import { io } from 'socket.io-client';
 
 const socket = io('/', { path: '/api/socketio' });
 export const SocketContext = createContext(socket);
-export const InitialStreamSettingsContext = createContext({});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isMuted, setIsMuted] = useState(false);
-  const [isVisible, setIsVisible] = useState(true);
-
   return (
     <UserProvider>
       <SocketContext.Provider value={socket}>
-        <InitialStreamSettingsContext.Provider
-          value={{
-            isMuted,
-            isVisible,
-            setIsMuted: () => setIsMuted(!isMuted),
-            setIsVisible: () => setIsVisible(!isVisible),
-          }}
-        >
-          <Component {...pageProps} />
-        </InitialStreamSettingsContext.Provider>
+        <Component {...pageProps} />
       </SocketContext.Provider>
     </UserProvider>
   );
