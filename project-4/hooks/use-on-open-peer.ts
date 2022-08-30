5a0bdc6a560dc0f7b99965cbd4f1fe8061706adb
olzh2102 <olzhas2102@yahoo.com>
2022-08-30 21:50:19 +0200

    refactor: change socket event name convention

--- project-4/hooks/use-on-open-peer.ts
+++ project-4/hooks/use-on-open-peer.ts
@@ -1,27 +1,28 @@
+import { useContext, useEffect, useState } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
 import Peer from 'peerjs';
-import { useContext, useEffect, useState } from 'react';
+
 import { SocketContext } from '@pages/_app';
-import useGetRoomId from './use-get-room-id';
 import { Nullable } from 'common/types';
+import useGetRoomId from './use-get-room-id';
 
 const useOnOpenPeer = (peer: Nullable<Peer>, isPeerMuted: boolean) => {
-  const roomId = useGetRoomId();
+  const { user } = useUser();
+
+  const room = useGetRoomId();
   const socket = useContext(SocketContext);
 
   const [me, setMe] = useState('');
-  const { user } = useUser();
 
   useEffect(() => {
     if (!peer || !socket || !user) return;
 
     peer.on('open', (id) => {
       setMe(id);
-      socket.emit('join-room', {
-        userId: id,
-        roomId,
-        username: user.name,
-        isPeerMuted,
+
+      socket.emit('room:join', {
+        room,
+        user: { id, name: user.name, muted: isPeerMuted },
       });
 
       console.log('Your device ID is: ', id);
@@ -30,11 +31,6 @@ const useOnOpenPeer = (peer: Nullable<Peer>, isPeerMuted: boolean) => {
     peer.on('error', (err) => {
       console.error('Peer connection error:', err);
     });
-
-    return () => {
-      peer.off('open');
-      peer.off('error');
-    };
   }, [peer, socket, user]);
 
   return me;
--- project-4/hooks/use-peer-on-join-room.ts
+++ project-4/hooks/use-peer-on-join-room.ts
@@ -15,41 +15,50 @@ const usePeerOnJoinRoom = (
   isMuted: boolean,
   setIsMuted: Dispatch<SetStateAction<KeyValue<boolean>>>
 ) => {
-  const { socket, peer, setPeers, user, stream } = useContext(QoraContext);
+  const {
+    socket,
+    peer,
+    setPeers,
+    user: myself,
+    stream,
+  } = useContext(QoraContext);
 
   useEffect(() => {
     if (!socket || !stream || !peer || !setPeers) return;
 
-    socket.on('member-joined', ({ userId, username, isPeerMuted }: any) => {
-      const call = peer.call(userId, stream, {
-        metadata: {
-          username: user?.name,
-          isMuted,
-        },
-      });
-      console.log('call friend with name:', username);
-      console.log('call friend with id:', userId);
+    socket.on(
+      'user:joined',
+      (user: { id: string; name: string; muted: boolean }) => {
+        const call = peer.call(user.id, stream, {
+          metadata: {
+            username: myself?.name,
+            isMuted,
+          },
+        });
+        console.log('call friend with name:', user.name);
+        console.log('call friend with id:', user.id);
 
-      call.on('stream', (friendStream: any) => {
-        console.log('friend stream');
-        userId &&
-          addVideoStream({
-            id: userId,
-            name: username,
-            stream: friendStream,
-          });
-      });
+        call.on('stream', (friendStream: any) => {
+          console.log('friend stream');
+          user.id &&
+            addVideoStream({
+              id: user.id,
+              name: user.name,
+              stream: friendStream,
+            });
+        });
 
-      call.on('close', () => {
-        toast(`${username} has left the room`);
-      });
+        call.on('close', () => {
+          toast(`${user.name} has left the room`);
+        });
 
-      setPeers((prevState: any) => ({ ...prevState, [userId]: call }));
-      setIsMuted((prev) => ({ ...prev, [userId]: isPeerMuted }));
-    });
+        setPeers((prevState: any) => ({ ...prevState, [user.id]: call }));
+        setIsMuted((prev) => ({ ...prev, [user.id]: user.muted }));
+      }
+    );
 
     return () => {
-      socket.off('member-joined');
+      socket.off('user:joined');
     };
   }, [socket, stream, peer, isMuted]);
 };
