d718e9de438d662e6edf0aa671fa93f6e9bf273d
olzh2102 <olzhas2102@yahoo.com>
2022-09-03 22:49:18 +0200

    fix: CrossLineDiv in Lobby page

--- project-4/common/components/cross-line-div.tsx
+++ project-4/common/components/cross-line-div.tsx
@@ -1,4 +1,4 @@
-const CrossLineDiv = ({ pos = { top: 6, left: 0.5 } }) => (
+const CrossLineDiv = ({ pos = { top: 6, left: '1/2' } }) => (
   <div
     className={'bg-current absolute w-2/3 h-0.5 -translate-x-1/2 -rotate-45 '.concat(
       Object.entries(pos)
