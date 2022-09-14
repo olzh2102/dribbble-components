363b3a4673c664b2923e334881bf242a0401a1bb
olzh2102 <olzhas2102@yahoo.com>
2022-09-14 22:19:14 +0200

    draft: from tailwind

--- project-4/components/control-panel/index.tsx
+++ project-4/components/control-panel/index.tsx
@@ -104,7 +104,21 @@ const ControlPanel = ({
         <Tooltip id="shareScreen" effect="solid" />
       </div>
 
-      <div className="w-9">
+      <div className="flex gap-4">
+        <span className="inline-block relative">
+          <span className="inline-block h-10 w-10 rounded-xl overflow-hidden bg-gray-100">
+            <svg
+              className="h-full w-full text-gray-300"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"></path>
+            </svg>
+          </span>
+          <span className="place-content-center absolute top-0 right-0 block h-4 w-4 transform -translate-y-1/2 translate-x-1/2 rounded-full bg-amber-300 text-xs text-center text-black">
+            5
+          </span>
+        </span>
         <button
           data-for="chat"
           data-tip="chat with everyone"
