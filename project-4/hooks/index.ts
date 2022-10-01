7bc30f927cb8fd2ed1e34d38052dc2fbf47df467
olzh2102 <olzhas2102@yahoo.com>
2022-10-01 09:44:38 +0200

    refactor: rename hooks

--- project-4/hooks/index.ts
+++ project-4/hooks/index.ts
@@ -1,15 +1,15 @@
 import usePeer from './use-peer';
 import useStream from './use-stream';
-import usePeerOnJoinRoom from './use-peer-on-join-room';
-import usePeerOnAnswer from './use-peer-on-answer';
+import useUserJoin from './use-user-join';
+import useUserAnswer from './use-user-answer';
 import useScreenShare from './use-screen-share';
 import useIsAudioActive from './use-is-audio-active';
 
 export {
   usePeer,
   useStream,
-  usePeerOnJoinRoom,
-  usePeerOnAnswer,
+  useUserJoin,
+  useUserAnswer,
   useScreenShare,
   useIsAudioActive,
 };
