c43e6ebdee26732b9b1dcb6fc11314ee7a36ffc3
evitla <s.dinmuhamed@gmail.com>
2022-10-08 14:11:27 +0600

    fix: do not remove video on cam hidden

--- project-4/components/video-plug/index.tsx
+++ project-4/components/video-plug/index.tsx
@@ -8,7 +8,7 @@ const VideoPlug = ({ userPicture }: { userPicture: string }) => {
       />
       <div className="rounded-[12px] bg-black/10 border border-gray-500 backdrop-blur-2xl absolute top-0 left-0 w-full h-full" />
       <img
-        className="rounded-full absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"
+        className="w-24 h-24 rounded-full absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"
         src={userPicture}
         alt="User image"
       />
