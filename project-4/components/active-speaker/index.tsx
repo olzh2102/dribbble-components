035b1cf795e4eb3c67956e1b05aa52e4fd9b1d7c
olzh2102 <olzhas2102@yahoo.com>
2022-10-26 15:53:39 +0200

    refactor: ActiveSpeaker component

--- project-4/components/active-speaker/index.tsx
+++ project-4/components/active-speaker/index.tsx
@@ -3,22 +3,11 @@ import React from 'react';
 import { SpeakerIcon } from 'assets/icons';
 import useIsAudioActive from '@hooks/use-is-audio-active';
 
-const ActiveSpeaker = ({ stream }: { stream: MediaStream }) => {
-  const isSpeaking = useIsAudioActive({ source: stream });
-
-  return isSpeaking ? (
-    <div
-      className={`
-        animate-[wiggle_1s_ease-in-out_infinite] 
-        rounded-full 
-        bg-indigo-400 
-        absolute top-3 right-3 
-        p-1
-    `}
-    >
+const ActiveSpeaker = ({ stream }: { stream: MediaStream }) =>
+  useIsAudioActive({ source: stream }) ? (
+    <div className="rounded-full bg-indigo-400 absolute top-3 right-3 p-1">
       <SpeakerIcon />
     </div>
   ) : null;
-};
 
 export default ActiveSpeaker;
