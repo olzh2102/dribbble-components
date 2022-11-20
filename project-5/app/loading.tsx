c809c103dea094679aa9c6cdcf16e078549bca94
evitla <s.dinmuhamed@gmail.com>
2022-11-20 20:10:17 +0600

    refactor: use pages folder instead of app folder

--- project-5/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="h-screen bg-slate-700 flex justify-center items-center">
-      <h2 className="text-white font-bold">Loading...</h2>
-    </div>
-  )
-}
--- project-5/pages/_app.tsx
+++ project-5/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ThemeProvider from '../contexts/theme-provider'
+
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
