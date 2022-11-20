fe0787f0077896695ee12f876010190756a9c612
evitla <s.dinmuhamed@gmail.com>
2022-11-20 19:00:26 +0600

    feat: add Loading component

---
+++ project-5/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h2 className="text-white font-bold">Loading...</h2>
+    </div>
+  )
+}
