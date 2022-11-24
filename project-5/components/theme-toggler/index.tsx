d40b6a671e4ad9c64a2aa6501fa1b09c0464af5e
evitla <s.dinmuhamed@gmail.com>
2022-11-24 11:02:39 +0600

    feat: add theme toggler animation

--- project-5/components/theme-toggler/index.tsx
+++ project-5/components/theme-toggler/index.tsx
@@ -1,19 +1,41 @@
 import React, { useContext } from 'react'
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import { motion } from 'framer-motion'
 
 import { ThemeContext } from '~contexts/theme-provider'
 
 export default function ThemeToggler() {
-  const { setTheme } = useContext(ThemeContext)
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  const variants = {
+    show: { opacity: 1, zIndex: 1, rotate: 0 },
+    hidden: { opacity: 0, zIndex: 0, rotate: 120 },
+  }
+
+  const transition = {
+    rotate: { type: 'spring', damping: 8, stiffness: 90 },
+  }
 
   return (
-    <>
-      <button className="hidden dark:inline" onClick={() => setTheme('light')}>
+    <div className="relative">
+      <motion.button
+        animate={theme === 'light' ? 'hidden' : 'show'}
+        variants={variants}
+        transition={transition}
+        className="absolute top-0 left-0"
+        onClick={() => setTheme('light')}
+      >
         <SunIcon className="h-6 w-6 text-white" />
-      </button>
-      <button className="dark:hidden" onClick={() => setTheme('dark')}>
+      </motion.button>
+      <motion.button
+        animate={theme === 'dark' ? 'hidden' : 'show'}
+        variants={variants}
+        transition={transition}
+        className="absolute top-0 left-0"
+        onClick={() => setTheme('dark')}
+      >
         <MoonIcon className="h-5 w-5 text-slate-900" />
-      </button>
-    </>
+      </motion.button>
+    </div>
   )
 }
