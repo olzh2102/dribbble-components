0f733e9a648779795edac567076976522e05440b
evitla <s.dinmuhamed@gmail.com>
2022-12-05 14:19:02 +0600

    feat: nav menu i18n

--- project-5/common/components/header/index.tsx
+++ project-5/common/components/header/index.tsx
@@ -1,22 +1,32 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
+import lang from 'common/lang.json'
+import { Lang } from 'common/types'
+
 export default function Header({ children }: { children: ReactNode }) {
+  const locale = useRouter().locale
+  const t = lang[locale as Lang]
+
   return (
     <header className="flex justify-between pointer-events-auto mt-4 items-center">
       <nav>
         <ul className="flex gap-6 ml-20 text-black dark:text-white">
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link href="/">{t.header_home}</Link>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <Link href="/projects">{t.header_projects}</Link>
           </li>
           <li>
-            <Link href="/services">Services</Link>
+            <Link href="/about">{t.header_about}</Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link href="/services">{t.header_services}</Link>
+          </li>
+          <li>
+            <Link href="/contact">{t.header_contact}</Link>
           </li>
         </ul>
       </nav>
--- project-5/contexts/lang-provider.tsx
+++ project-5/contexts/lang-provider.tsx
@@ -1,19 +1,17 @@
-import { Lang, LangContextType } from "common/types";
-import { useRouter } from "next/router";
-import { createContext, ReactNode } from "react";
+import { Lang } from 'common/types'
+import { useRouter } from 'next/router'
+import { createContext, ReactNode } from 'react'
 
 export const LangContext = createContext<any>({
   setLang: () => {},
-});
+})
 
 export default function LangProvider({ children }: { children: ReactNode }) {
-  const router = useRouter();
+  const router = useRouter()
 
   function setLang(lang: Lang) {
-    router.push("/", "/", { locale: lang });
+    router.push(router.route, undefined, { locale: lang })
   }
 
-  return (
-    <LangContext.Provider value={setLang}>{children}</LangContext.Provider>
-  );
+  return <LangContext.Provider value={setLang}>{children}</LangContext.Provider>
 }
