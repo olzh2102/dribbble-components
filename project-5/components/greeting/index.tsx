ae6ad5639bf627dd7923d8b095e6ca047b083df0
evitla <s.dinmuhamed@gmail.com>
2022-12-06 11:09:59 +0600

    test: Greeting component

---
+++ project-5/components/greeting/index.tsx
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router'
+
+import lang from 'common/lang.json'
+import { Lang } from 'common/types'
+import { getTimeOfDay } from 'common/utils'
+
+export default function Greeting() {
+  const locale = useRouter().locale
+  const t = lang[locale as Lang]
+  const timeOfDay = getTimeOfDay()
+
+  return (
+    <div className="grid h-full place-content-center text-5xl dark:text-white dark:mix-blend-exclusion">
+      {t.greeting[timeOfDay]}
+    </div>
+  )
+}
