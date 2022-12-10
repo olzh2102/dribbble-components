ea2377c63e2de74908ba9b9ebd1d70b9ddc7eb7c
olzh2102 <olzhas2102@yahoo.com>
2022-12-10 21:33:59 +0100

    refactor: remove unused code

--- project-5/common/types/index.ts
+++ project-5/common/types/index.ts
@@ -2,11 +2,6 @@ import { NextPage } from 'next'
 
 export type Lang = 'en' | 'de' | 'ru'
 
-export type LangContextType = {
-  lang: Lang
-  setLang: (lang: Lang) => void
-}
-
 export type Theme = 'light' | 'dark'
 export type Page = NextPage & { waveBackground?: boolean }
 
