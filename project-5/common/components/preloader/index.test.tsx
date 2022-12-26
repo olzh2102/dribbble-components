9919104e2c90e7f8f5a40aee5f92a29ee54fd527
evitla <s.dinmuhamed@gmail.com>
2022-12-26 14:00:23 +0600

    refactor: remove async/await

--- project-5/common/components/preloader/index.test.tsx
+++ project-5/common/components/preloader/index.test.tsx
@@ -7,13 +7,13 @@ describe('Preloader', () => {
 
   afterAll(() => jest.useRealTimers())
 
-  it('should call setLoading after duration time passed', async () => {
+  it('should call setLoading after duration time passed', () => {
     const setLoading = jest.fn()
     render(<Preloader duration={2000} setLoading={setLoading} />)
 
     expect(setLoading).not.toHaveBeenCalled()
 
-    await act(() => {
+    act(() => {
       jest.advanceTimersByTime(1000)
       expect(setLoading).not.toHaveBeenCalled()
 
--- project-5/hooks/use-counter.test.ts
+++ project-5/hooks/use-counter.test.ts
@@ -7,7 +7,7 @@ describe('useCounter', () => {
 
   afterAll(() => jest.useRealTimers())
 
-  it('should use counter', async () => {
+  it('should use counter', () => {
     const from = 0
     const to = 100
     const duration = 4000
@@ -15,13 +15,13 @@ describe('useCounter', () => {
 
     expect(result.current).toBe(from)
 
-    await act(() => {
+    act(() => {
       jest.advanceTimersByTime(duration / 2)
     })
 
     expect(result.current).toBe((to - from) / 2)
 
-    await act(() => {
+    act(() => {
       jest.advanceTimersByTime(duration / 2)
     })
 
