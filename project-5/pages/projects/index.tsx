621497eb9ec9cd8f11e3d37c78a50de3793f2f50
evitla <s.dinmuhamed@gmail.com>
2023-02-08 21:06:40 +0600

    try: projects layout

--- project-5/pages/projects/index.tsx
+++ project-5/pages/projects/index.tsx
@@ -1,7 +1,23 @@
 import withLayout from 'common/components/layout/with-layout'
 
 const Projects = () => {
-  return <div>Projects</div>
+  const LEN = 4
+  const projects = Array.from({ length: LEN })
+
+  return (
+    <div className="p-2 h-full flex flex-col gap-2">
+      <div className="flex gap-2 h-1/2">
+        {projects.slice(0, Math.floor(LEN / 2)).map((_, i) => (
+          <div key={i} className="bg-blue-400 rounded flex-auto" />
+        ))}
+      </div>
+      <div className="flex gap-2 h-1/2">
+        {projects.slice(Math.floor(LEN / 2)).map((_, i) => (
+          <div key={i} className="bg-blue-400 rounded flex-auto" />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default withLayout(Projects)
