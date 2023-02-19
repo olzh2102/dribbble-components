4485655e939a27abf358673a536b710b47250b59
evitla <s.dinmuhamed@gmail.com>
2023-02-19 15:04:46 +0600

    feat: ability to show message on cursor hover

--- project-5/pages/projects/index.tsx
+++ project-5/pages/projects/index.tsx
@@ -1,32 +1,43 @@
+import { useContext } from 'react'
+
 import Image from 'next/image'
 
 import { motion } from 'framer-motion'
 
 import withLayout from '~components/layout/with-layout'
+import { CursorContext } from '~contexts/cursor-provider'
 
-const Projects = () => (
-  <div className="flex flex-col h-full">
-    <Image
-      src="/nr-logo.svg"
-      width={40}
-      height={40}
-      alt="logo"
-      className="absolute mix-blend-difference m-2"
-    />
-    <div className="flex justify-center gap-4 my-auto">
-      {[1, 2, 3, 4].map((i) => (
-        <motion.img
-          key={i}
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ damping: 50, stiffness: 100 }}
-          src="https://picsum.photos/300/500"
-          alt="Picture of project"
-          className="rounded"
-        />
-      ))}
+const Projects = () => {
+  const { onMouseOver, onMouseOut } = useContext(CursorContext)
+
+  return (
+    <div className="flex flex-col h-full">
+      <Image
+        src="/nr-logo.svg"
+        width={40}
+        height={40}
+        alt="logo"
+        className="absolute mix-blend-difference m-2"
+      />
+      <div className="flex justify-center gap-4 my-auto">
+        {[1, 2, 3, 4].map((i) => (
+          <motion.img
+            key={i}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ damping: 50, stiffness: 100 }}
+            onMouseOver={(e) =>
+              onMouseOver(e, 'img', { message: <span className="text-[3px]">Zhailau {i}</span> })
+            }
+            onMouseOut={(e) => onMouseOut(e, 'img')}
+            src="https://picsum.photos/300/500"
+            alt="Picture of project"
+            className="rounded"
+          />
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default withLayout(Projects)
