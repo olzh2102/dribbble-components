49fe49a9ccd8d3637c21397cc1b81d8dfbc1baf9
olzh2102 <olzhas2102@yahoo.com>
2023-03-05 09:58:35 +0100

    draft: contact form

--- project-5/pages/contact/index.tsx
+++ project-5/pages/contact/index.tsx
@@ -1,7 +1,60 @@
+import { useState, ChangeEvent } from 'react'
 import withLayout from '~components/layout/with-layout'
 
 const Contact = () => {
-  return <div>Contact</div>
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    details: '',
+  })
+
+  function setValue(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  return (
+    <form className="h-full flex">
+      <div className="w-2/5 bg-red-300 h-full rounded-l-md"></div>
+      <div className="w-3/5 h-full grid place-content-center">
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={formState.name}
+              onChange={setValue}
+            />
+          </label>
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={setValue}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Project details
+            <textarea
+              name="details"
+              value={formState.details}
+              onChange={setValue}
+            />
+          </label>
+        </div>
+        <button className="block ml-auto" type="submit">
+          Send
+        </button>
+      </div>
+    </form>
+  )
 }
 
 export default withLayout(Contact)
