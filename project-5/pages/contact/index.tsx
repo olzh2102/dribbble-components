41d7eeea426370bc78b599fca95628b46ae93c89
olzh2102 <olzhas2102@yahoo.com>
2023-03-05 22:02:38 +0100

    draft: switches on left side

--- project-5/pages/contact/index.tsx
+++ project-5/pages/contact/index.tsx
@@ -17,7 +17,23 @@ const Contact = () => {
 
   return (
     <form className="h-full flex">
-      <div className="w-2/5 bg-red-300 h-full rounded-l-md"></div>
+      <div className="w-2/5 bg-red-300 h-full rounded-l-md grid place-content-center">
+        <div className="flex gap-2">
+          <h2>What can I do for you?</h2>
+          <div className="uppercase w-[150px] h-[150px] rounded border border-blue-500 grid place-content-center">
+            Design
+          </div>
+          <div className="uppercase w-[150px] h-[150px] rounded border border-blue-500 grid place-content-center">
+            Branding
+          </div>
+          <div className="uppercase w-[150px] h-[150px] rounded border border-blue-500 grid place-content-center">
+            Consulting
+          </div>
+        </div>
+        <div>
+          <h2>Budget in EUR</h2>
+        </div>
+      </div>
       <div className="w-3/5 h-full grid place-content-center">
         <div>
           <label>
