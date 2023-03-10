e5445b52a6c4254afe1a2ba4d6286955e481e6c2
evitla <s.dinmuhamed@gmail.com>
2023-03-10 14:39:50 +0600

    feat: email js

--- project-5/pages/contact/index.tsx
+++ project-5/pages/contact/index.tsx
@@ -1,8 +1,23 @@
+import { send } from '@emailjs/browser'
+
 import ContactForm from '~components/contact-form'
 import withLayout from '~components/layout/with-layout'
 
 const Contact = () => {
-  return <ContactForm onSubmit={console.log} />
+  async function sendEmail(data: any) {
+    const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID as string
+    const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID as string
+    const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY as string
+
+    try {
+      const result = await send(serviceId, templateId, data, publicKey)
+      console.log(result)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return <ContactForm onSubmit={sendEmail} />
 }
 
 export default withLayout(Contact)
