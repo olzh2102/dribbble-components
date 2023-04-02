0dd716a6a2b2d81a03b75d4ad5b6e7f2f408526c
olzh2102 <olzhas2102@yahoo.com>
2023-04-02 08:42:07 +0200

    refactor: projects page

--- project-5/pages/projects/index.tsx
+++ project-5/pages/projects/index.tsx
@@ -1,53 +1,46 @@
-import { useContext } from 'react'
-
 import Image from 'next/image'
 
 import { imageLoader } from 'common/utils'
 import withLayout from '~components/layout/with-layout'
-import { CursorContext } from '~contexts/cursor-provider'
 
 const PROJECTS = ['project-1', 'project-2', 'project-3', 'project-4']
 
 const Projects = () => {
-  const { onMouseOver, onMouseOut } = useContext(CursorContext)
-
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full p-2">
       <Image
         src="/nr-logo.svg"
-        width={40}
-        height={40}
+        width="40"
+        height="40"
         alt="logo"
-        className="absolute mix-blend-difference m-2"
+        className="absolute mix-blend-difference"
       />
 
       <div className="flex justify-center gap-8 my-auto">
         {PROJECTS.map((project, i) => (
-          <Image
+          <div
+            className="flex flex-col text-primary-zinc dark:text-primary-milk uppercase"
             key={i}
-            loader={imageLoader}
-            src={`${project}.jpg`}
-            width="270"
-            height="600"
-            alt="Project picture"
-            className="rounded grayscale hover:grayscale-0"
-            onMouseOver={(e) =>
-              onMouseOver(e, 'img', {
-                message: (
-                  <div className="flex flex-col justify-between">
-                    <h3 className="text-[2px] uppercase font-medium">
-                      {project}
-                    </h3>
-                    <span className="text-[1.5px]">Square meters: 78 sqm</span>
-                    <span className="text-[1.5px]">
-                      Design Style: Scandinavian
-                    </span>
-                  </div>
-                ),
-              })
-            }
-            onMouseOut={(e) => onMouseOut(e, 'img')}
-          />
+          >
+            <h2>{project}</h2>
+
+            <div className="rounded overflow-hidden">
+              <Image
+                key={i}
+                loader={imageLoader}
+                src={`${project}.jpg`}
+                width="280"
+                height="700"
+                alt="Project picture"
+                className="grayscale hover:grayscale-0 hover:scale-105 transition-all duration-500"
+              />
+            </div>
+
+            <span className="text-sm text-right">
+              design style: scandinavian
+            </span>
+            <span className="text-sm text-right">square meters: 78 sqm</span>
+          </div>
         ))}
       </div>
     </div>
