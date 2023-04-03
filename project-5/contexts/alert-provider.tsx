39049c0e9ec9ccba050d01e84c0e0882fb430ea0
evitla <s.dinmuhamed@gmail.com>
2023-04-03 16:13:48 +0600

    feat: snackbar severity icon

--- project-5/contexts/alert-provider.tsx
+++ project-5/contexts/alert-provider.tsx
@@ -1,8 +1,15 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react'
 
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/24/solid'
+
 import Snackbar from '~components/snackbar'
 
-type Alert = { show: boolean; severity?: 'success' | 'error' | 'info'; message?: string }
+type Severity = 'success' | 'error' | 'info'
+type Alert = { show: boolean; severity?: Severity; message?: string }
 
 const alertStyle = {
   success: 'bg-[#508991]',
@@ -17,7 +24,7 @@ export const AlertContext = createContext<{
 })
 
 export default function AlertProvider({ children }: { children: ReactNode }) {
-  const [alert, setAlert] = useState<Alert>({ show: true, message: 'TRALALA' })
+  const [alert, setAlert] = useState<Alert>({ show: false })
 
   return (
     <AlertContext.Provider value={{ setAlert }}>
@@ -25,12 +32,21 @@ export default function AlertProvider({ children }: { children: ReactNode }) {
       <Snackbar open={alert.show} onClose={() => setAlert({ show: false })} autoHideDuration={3000}>
         <div
           className={
-            'px-3 py-4 rounded text-white font-medium w-72 ' + alertStyle[alert.severity || 'info']
+            'px-3 py-4 rounded text-white font-medium w-72 flex items-center gap-x-2 ' +
+            alertStyle[alert.severity || 'info']
           }
         >
-          {alert.message}
+          {alert.severity && <SeverityIcon severity={alert.severity} />}
+          <span>{alert.message}</span>
         </div>
       </Snackbar>
     </AlertContext.Provider>
   )
+}
+
+function SeverityIcon({ severity }: { severity: Severity }) {
+  if (severity === 'error') return <ExclamationTriangleIcon className="w-5 h-5" />
+  if (severity === 'success') return <CheckCircleIcon className="w-5 h-5" />
+
+  return <InformationCircleIcon className="w-5 h-5" />
 }
