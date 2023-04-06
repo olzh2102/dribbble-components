d814fe81353f063d807973dc247a8f80845241c9
evitla <s.dinmuhamed@gmail.com>
2023-04-06 17:13:07 +0600

    refactor: use clsx for link element

--- project-5/components/header/menu-item.tsx
+++ project-5/components/header/menu-item.tsx
@@ -1,43 +1,30 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+import clsx from 'clsx'
 import { motion } from 'framer-motion'
 
 import lang from 'common/lang.json'
-import { Lang, RoutePath, TranslationKey } from 'common/types'
+import { RoutePath } from 'common/types'
 
-const activeVariants = {
-  common:
-    'bg-gradient-to-l bg-clip-text text-transparent from-action-peach dark:from-action-gold',
-  home: 'via-primary-zinc to-primary-zinc dark:via-primary-milk dark:to-primary-milk',
-  away: 'via-primary-milk to-primary-milk dark:via-primary-zinc dark:to-primary-zinc',
-}
-
-export default function MenuItem({
-  route,
-  mobile,
-}: {
-  route: RoutePath
-  mobile?: boolean
-}) {
+export default function MenuItem({ route, mobile }: { route: RoutePath; mobile?: boolean }) {
   const { locale, route: currentRoute } = useRouter()
-  const t = lang[locale as Lang]
+  const t = lang[locale]
 
-  const translationKey =
-    route === '/' ? 'home' : (route.replace('/', '') as TranslationKey)
+  const translationKey = route === '/' ? 'home' : route.replace('/', '')
+
+  const linkStyles = clsx({
+    'bg-gradient-to-l bg-clip-text text-transparent from-action-peach dark:from-action-gold':
+      route === currentRoute,
+    'via-primary-zinc to-primary-zinc dark:via-primary-milk dark:to-primary-milk':
+      route === '/' || mobile,
+    'via-primary-milk to-primary-milk dark:via-primary-zinc dark:to-primary-zinc':
+      route !== '/' && !mobile,
+  })
 
   return (
     <li className="relative w-min">
-      <Link
-        href={route}
-        className={
-          route === currentRoute
-            ? activeVariants.common +
-              ' ' +
-              activeVariants[route === '/' || mobile ? 'home' : 'away']
-            : ''
-        }
-      >
+      <Link href={route} className={linkStyles}>
         {t.header[translationKey]}
       </Link>
 
@@ -46,11 +33,7 @@ export default function MenuItem({
         animate={route === currentRoute ? 'show' : 'hidden'}
         transition={{ opacity: { duration: 1.2, type: 'spring' } }}
         variants={{ show: { opacity: 1 }, hidden: { opacity: 0 } }}
-        className={`
-          absolute top-2 -right-3 
-          w-1 h-1 rounded-full bg-action-peach dark:bg-action-gold
-          shadow-active-menu-item
-        `}
+        className="absolute top-2 -right-3 w-1 h-1 rounded-full bg-action-peach dark:bg-action-gold shadow-active-menu-item"
       />
     </li>
   )
