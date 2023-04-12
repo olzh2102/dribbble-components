9367a262f7a2649f2847eb6a4ba53c68fb4885a1
evitla <s.dinmuhamed@gmail.com>
2023-04-12 17:50:06 +0600

    chore: second page

--- project-5/pages/projects/[project].tsx
+++ project-5/pages/projects/[project].tsx
@@ -46,10 +46,33 @@ const Project = () => {
       </div>
 
       <div className="flex flex-[0_0_100%] text-primary-zinc dark:text-primary-milk">
-        <div className="w-full grid place-content-center p-2">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias sit eius perspiciatis
-          sint fugit, delectus maiores accusantium doloremque. Nulla sunt ipsum vero sit enim
-          inventore officiis similique odio, facilis eos.
+        <Image
+          loader={imageLoader}
+          src={`project-2.jpg`}
+          width="1000"
+          height="2000"
+          alt="Profile picture"
+          className="md:w-5/12 md:rounded-r rounded-t md:rounded-tl-none object-cover object-left"
+        />
+        <div className="w-full flex flex-col px-2 py-5">
+          <div className="flex-1 relative">
+            <Image
+              loader={imageLoader}
+              src={`project-3.jpg`}
+              width="300"
+              height="200"
+              alt="Profile picture"
+              className="rounded absolute top-40 right-40"
+            />
+          </div>
+          <div>
+            <h2 className="text-5xl uppercase font-semibold italic">Living Room</h2>
+            <p className="text-lg">
+              Living room design refers to the arrangement and styling of furniture, decor,
+              lighting, and other elements in a space typically used for socializing, relaxation,
+              and entertainment.
+            </p>
+          </div>
         </div>
       </div>
     </ScrollWrapper>
