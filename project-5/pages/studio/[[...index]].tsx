e6e4ca9c77bd16ee8888110ecdd314ec3809584c
olzh2102 <olzhas2102@yahoo.com>
2023-05-21 08:50:23 +0200

    test

--- project-5/pages/studio/[[...index]].tsx
+++ project-5/pages/studio/[[...index]].tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+
 import { NextStudio } from 'next-sanity/studio'
 import { metadata } from 'next-sanity/studio/metadata'
+
 import config from '../../sanity.config'
 
 export default function StudioPage() {
--- project-5/sanity.config.ts
+++ project-5/sanity.config.ts
@@ -2,24 +2,24 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/pages/studio/[[...index]].tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from './sanity/env'
+import { schema } from './sanity/schema'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: '2023-05-21' }),
   ],
 })
