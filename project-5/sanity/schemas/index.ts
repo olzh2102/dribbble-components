ba873f88163be90287bd175fa8dd79f941ff05d2
olzh2102 <olzhas2102@yahoo.com>
2023-05-21 09:39:18 +0200

    test: project schema

---
+++ project-5/sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import { SchemaTypeDefinition } from 'sanity'
+
+import project from './project'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [project],
+}
---
+++ project-5/sanity/schemas/project.ts
@@ -0,0 +1,26 @@
+const project = {
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name' },
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+}
+
+export default project
