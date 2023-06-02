cd38e55edf133c33c42d663bf2c6fbc954607f08
olzh2102 <olzhas2102@yahoo.com>
2023-06-02 16:54:55 +0200

    feat: integrate sanity

---
+++ project-5/sanity.config.ts
@@ -0,0 +1,13 @@
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+
+import { schema } from './sanity/schema'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId: '0ptjayg8',
+  dataset: 'production',
+  schema,
+  plugins: [deskTool(), visionTool({ defaultApiVersion: '2023-06-02' })],
+})
---
+++ project-5/sanity/schemas/index.ts
@@ -0,0 +1,30 @@
+import { SchemaTypeDefinition } from 'sanity'
+
+const supportedLanguages = [
+  { id: 'en', title: 'English', isDefault: true },
+  { id: 'de', title: 'German' },
+  { id: 'ru', title: 'Russian' },
+]
+
+const localeString = {
+  title: 'Localized string',
+  name: 'localeString',
+  type: 'object',
+  fieldsets: [
+    {
+      title: 'Translations',
+      name: 'translations',
+      options: { collapsible: true },
+    },
+  ],
+  fields: supportedLanguages.map((lang) => ({
+    title: lang.title,
+    name: lang.id,
+    type: 'string',
+    options: lang.isDefault ? null : 'translations',
+  })),
+}
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [localeString],
+}
